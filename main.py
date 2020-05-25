ed2b52948e155e42788c30792f9d0f84a3dc7db9
Kabila Haile Soboka <kabilahaile@gmail.com>
2020-05-25 21:27:20 +0300

    Get the position and handle of the game window

--- main.py
+++ main.py
@@ -3,6 +3,26 @@ import numpy as np
 from PIL import ImageGrab
 import win32gui
 
+toplist = []
+windowList = []
+def enum_win(windowHandle, result):
+    windowText = win32gui.GetWindowText(windowHandle)
+    windowList.append((windowHandle,windowText))
+
+#Get the list of open windows and p
+win32gui.EnumWindows(enum_win, toplist)
+
+#To get the handle of the game
+gameHandle = 0
+for (handle,text) in windowList:
+    if 'chrome://dino/' in text:
+        gameHandle = handle
+if gameHandle != 0:
+    pass
+
+#Get the extact position of the window
+position = win32gui.GetWindowRect(gameHandle)
+print(position)
 #Take screenshot
-screenshot = ImageGrab.grab(bbox=(0 ,0 , 500, 500)) #give the location to take screenshot from
-screenshot.show()
+#screenshot = ImageGrab.grab(bbox=(0 ,0 , 500, 500)) #give the location to take screenshot from
+#screenshot.show()
