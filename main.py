0309c5bb21354eaf561bf8ab9bcb28b3de04f2d0
Kabila Haile Soboka <kabilahaile@gmail.com>
2020-05-26 19:32:39 +0300

    HSV Tracking

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import win32gui #To access the Windows GUI
 import win32api #To get api of the elements
 import win32con
 import time
+import imutils
 toplist = []
 windowList = []
 
@@ -49,9 +50,14 @@ while True:
     low = np.array([0,0,166])
     high = np.array([0,0,172])
     mask = cv2.inRange(screenshot_array,low,high)
-    contours , location = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+    contours = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
     for i in contours:
         area = cv2.contourArea(i)
+        M = cv2.moments(i)
+        cx = int(M['m00'])
+        cy = int(M['m01'])
+        cv2.circle(screenshot_array2,(cx,cy),7,(255,255,255),-1)
         if area > 50:
             cv2.drawContours(screenshot_array2,i,-1,(0,255,0),3)
     #Show the image
