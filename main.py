f9370ee878fd18e69469d9563249f8374355a090
Kabila Haile Soboka <kabilahaile@gmail.com>
2020-05-27 02:40:31 +0300

    Down for birds and jump for cactus

--- main.py
+++ main.py
@@ -27,24 +27,28 @@ if gameHandle != 0:
 else:
     print('Window not available')
     exit(-1)
-#Begin the game
-win32api.SendMessage(gameHandle, win32con.WM_KEYDOWN, win32con.VK_SPACE)
-win32api.SendMessage(gameHandle, win32con.WM_KEYUP, win32con.VK_SPACE)
-time.sleep(2)
 #Resize the window
 win32gui.MoveWindow(gameHandle, 0, 0,788,498, True)
-#Set the game window to forground
-win32gui.SetForegroundWindow(gameHandle)
 kernelOpen = np.ones((3,3))
-kernelClose = np.ones((10,10))
+kernelClose = np.ones((12,12))
 #Get the extact position of the window
 isFirst = True
 firstX = 0
+#Set the game window to forground
+win32gui.SetForegroundWindow(gameHandle)
+#Reload the game
+win32api.SendMessage(gameHandle, win32con.WM_KEYDOWN, win32con.VK_F5)
+win32api.SendMessage(gameHandle, win32con.WM_KEYUP, win32con.VK_F5)
+time.sleep(1)
+win32api.SendMessage(gameHandle, win32con.WM_KEYDOWN, win32con.VK_SPACE)
+win32api.SendMessage(gameHandle, win32con.WM_KEYUP, win32con.VK_SPACE)
+time.sleep(4)
+
 while True:
     position = win32gui.GetWindowRect(gameHandle)
-    print(position)
+    x,y,w,h = position
     #Take screenshot
-    screenshot = ImageGrab.grab(bbox=position) #give the location to take screenshot from
+    screenshot = ImageGrab.grab(bbox=(x,y,w-200,h)) #give the location to take screenshot from
     #Convert the screenshot to numpy array
     
     screenshot_main = np.array(screenshot)
@@ -59,30 +63,35 @@ while True:
     maskOpen = cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernelOpen)
     maskClose = cv2.morphologyEx(maskOpen,cv2.MORPH_CLOSE,kernelClose)
     contours, h = cv2.findContours(maskClose.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-    xs = []
+    xs = dict()
     for i in range(len(contours)):
         x,y,w,h = cv2.boundingRect(contours[i])
         if isFirst:
-            width = x+w
-            height = y+h
+            dinoWidth = x+w
+            dinoY = y
             isFirst = False
-        cv2.rectangle(screenshot_array2,(x,y),(x+w,y+h),(0,0,255),2)
-        if x+w == width:
-            index = i
-            cv2.putText(screenshot_array2,"Dino"+str(x+w),(x,y+h+20),cv2.FONT_HERSHEY_COMPLEX,1,(233,244,255))
+        if x+w == dinoWidth:
+            cv2.rectangle(screenshot_array2,(x,y),(x+w,y+h),(0,255,0),2)
+            cv2.putText(screenshot_array2,"Dino",(x,y+h+20),cv2.FONT_HERSHEY_COMPLEX,1,(233,244,255))
         elif(len(contours)>1):
-                xs.append(x)
-                cv2.putText(screenshot_array2,str(i),(x,y+h+20),cv2.FONT_HERSHEY_COMPLEX,1,(233,244,255))
-    for i in xs:
-        if(i <= width+100):
-            win32api.SendMessage(gameHandle, win32con.WM_KEYDOWN, win32con.VK_SPACE)
-            win32api.SendMessage(gameHandle, win32con.WM_KEYUP, win32con.VK_SPACE)
-    
+                xs[i] = [x,y]
+                cv2.rectangle(screenshot_array2,(x,y),(x+w,y+h),(0,0,255),4)
+                cv2.putText(screenshot_array2,str(h),(x,y+h+20),cv2.FONT_HERSHEY_COMPLEX,1,(233,244,255))
+    for i in xs.values():
+        if((i[0] <= dinoWidth+110) and((dinoY < i[1]) or (((dinoY - i[1])<=8)and((dinoY - i[1])>=0)))):
+                win32api.SendMessage(gameHandle, win32con.WM_KEYDOWN, win32con.VK_SPACE)
+                win32api.SendMessage(gameHandle, win32con.WM_KEYUP, win32con.VK_SPACE)
+                print(i[1],dinoY)    
+        elif((i[0] <= dinoWidth+110) and(((i[1]-dinoY)<0))):
+                print("Bird",i[1],dinoY)
+                win32api.SendMessage(gameHandle, win32con.WM_KEYDOWN, win32con.VK_DOWN)
+                time.sleep(0.4)
+                win32api.SendMessage(gameHandle, win32con.WM_KEYUP, win32con.VK_DOWN)
     #cv2.WINDOW_NORMAL makes the output window resizealbe
     cv2.namedWindow('Screen', cv2.WINDOW_NORMAL)
  
     #resize the window according to the screen resolution
-    cv2.resizeWindow('Screen', 680, 440)
+    cv2.resizeWindow('Screen', 480, 240)
     #Show the image
     cv2.imshow("Screen",screenshot_array2)
     #Wait for 25 milisecond to take another
